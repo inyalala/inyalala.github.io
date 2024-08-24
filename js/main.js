661c1975fc92f2c55e1b1c713f7c784b86a9cfbf
Innocent Nyalala <11386133+nyalalainnocent@users.noreply.github.com>
2024-08-24 16:58:59 +0300

    On

---
+++ js/main.js
@@ -0,0 +1,96 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	var getHeight = function() {
+		var extraHeight = 0;
+
+		if ( isMobile.any() ) extraHeight = 50;
+		
+		setTimeout(function(){
+			$('#fh5co-main').stop().animate({
+				'height': $('.fh5co-tab-content.active').height() + extraHeight
+			});
+		}, 200);
+	};
+
+	var pieChart = function() {
+		$('.chart').easyPieChart({
+			scaleColor: false,
+			lineWidth: 10,
+			lineCap: 'butt',
+			barColor: '#17e7a4',
+			trackColor:	"#000000",
+			size: 160,
+			animate: 1000
+		});
+	};
+
+	var tabContainer = function() {
+		getHeight();
+		$(window).resize(function(){
+			getHeight();
+		})
+	};
+
+	var tabClickTrigger = function() {
+		$('.fh5co-tab-menu a').on('click', function(event) {
+			event.preventDefault();
+			var $this = $(this),
+				data = $this.data('tab'),
+				pie = $this.data('pie');
+	
+			// add/remove active class
+			$('.fh5co-tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active');
+	
+			// hide previously active content
+			$('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp').hide();
+	
+			// show the selected content
+			var selectedContent = $('.fh5co-tab-content[data-content="'+data+'"]');
+			selectedContent.addClass('active animated fadeInUp').show();
+	
+			getHeight();
+	
+			if (pie === 'yes') {
+				setTimeout(function(){
+					pieChart();
+				}, 800);
+			}
+		});
+	};
+	
+
+	// Document on load.
+	$(function(){
+		tabContainer();
+		tabClickTrigger();
+
+	});
+
+
+}());
