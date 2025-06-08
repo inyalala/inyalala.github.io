fc5a574aa267d6a95782b87b7fb868f062a76084
Innocent Nyalala <11386133+inyalala@users.noreply.github.com>
2025-06-08 13:42:55 +0300

    Deploy saail-lab to subdirectory

--- saail-lab/astro.config.mjs
+++
@@ -1,9 +0,0 @@
- 
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://inyalala.github.io',
-  base: '/saail-lab',
-  outDir: './dist'
-});
