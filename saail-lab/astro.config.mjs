cd1864b9bbaeb223bc1c56cf84ebdf0bd3d91ed7
Innocent Nyalala <11386133+inyalala@users.noreply.github.com>
2025-06-08 14:02:15 +0300

    Fix build configuration and redeploy saail-lab

---
+++ saail-lab/astro.config.mjs
@@ -0,0 +1,9 @@
+ 
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://inyalala.github.io',
+  base: '/saail-lab',
+  outDir: './dist'
+});
